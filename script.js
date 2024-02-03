955592f8480a3e2af877c9a66d8a885d4ee6959d
Monica Judith Medina <itsmo26@yahoo.com>
2024-02-02 19:35:51 -0600

    All players done

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const playerContainer = document.getElementById('all-players-container');
-const newPlayerFormContainer = document.getElementById('new-player-form');
+const playerContainer = document.getElementById("all-players-container");
+const newPlayerFormContainer = document.getElementById("new-player-form");
 
 // Add your cohort name to the cohortName variable below, replacing the 'COHORT-NAME' placeholder
-const cohortName = 'YOUR COHORT NAME HERE';
+const cohortName = "2308-ACC-ET-WEB-PT-A";
 // Use the APIURL variable for fetch requests
 const APIURL = `https://fsa-puppy-bowl.herokuapp.com/api/${cohortName}/`;
 
@@ -11,86 +11,105 @@ const APIURL = `https://fsa-puppy-bowl.herokuapp.com/api/${cohortName}/`;
  * @returns An array of objects.
  */
 const fetchAllPlayers = async () => {
-    try {
-
-    } catch (err) {
-        console.error('Uh oh, trouble fetching players!', err);
-    }
+  try { 
+    // const response = await fetch(`${BASE_URL}/${name}`);
+    const response = await fetch(`${APIURL}/players`);
+    const result = await response.json();
+    // console.log(result)
+    return result.data.players
+  } catch (err) {
+    console.error("Uh oh, trouble fetching players!", err);
+  }
 };
 
 const fetchSinglePlayer = async (playerId) => {
-    try {
-
-    } catch (err) {
-        console.error(`Oh no, trouble fetching player #${playerId}!`, err);
-    }
+  try {
+  } catch (err) {
+    console.error(`Oh no, trouble fetching player #${playerId}!`, err);
+  }
 };
 
 const addNewPlayer = async (playerObj) => {
-    try {
-
-    } catch (err) {
-        console.error('Oops, something went wrong with adding that player!', err);
-    }
+  try {
+  } catch (err) {
+    console.error("Oops, something went wrong with adding that player!", err);
+  }
 };
 
 const removePlayer = async (playerId) => {
-    try {
-
-    } catch (err) {
-        console.error(
-            `Whoops, trouble removing player #${playerId} from the roster!`,
-            err
-        );
-    }
+  try {
+  } catch (err) {
+    console.error(
+      `Whoops, trouble removing player #${playerId} from the roster!`,
+      err
+    );
+  }
 };
 
 /**
  * It takes an array of player objects, loops through them, and creates a string of HTML for each
- * player, then adds that string to a larger string of HTML that represents all the players. 
- * 
- * Then it takes that larger string of HTML and adds it to the DOM. 
- * 
- * It also adds event listeners to the buttons in each player card. 
- * 
- * The event listeners are for the "See details" and "Remove from roster" buttons. 
- * 
+ * player, then adds that string to a larger string of HTML that represents all the players.
+ *
+ * Then it takes that larger string of HTML and adds it to the DOM.
+ *
+ * It also adds event listeners to the buttons in each player card.
+ *
+ * The event listeners are for the "See details" and "Remove from roster" buttons.
+ *
  * The "See details" button calls the `fetchSinglePlayer` function, which makes a fetch request to the
- * API to get the details for a single player. 
- * 
+ * API to get the details for a single player.
+ *
  * The "Remove from roster" button calls the `removePlayer` function, which makes a fetch request to
- * the API to remove a player from the roster. 
- * 
+ * the API to remove a player from the roster.
+ *
  * The `fetchSinglePlayer` and `removePlayer` functions are defined in the
  * @param playerList - an array of player objects
  * @returns the playerContainerHTML variable.
  */
 const renderAllPlayers = (playerList) => {
-    try {
-        
-    } catch (err) {
-        console.error('Uh oh, trouble rendering players!', err);
-    }
+  try { 
+    const template = playerList.map(player => {
+        return (
+            `<section>
+            <h2>${player.name}</h2>
+            </section>`
+        )
+    }).join('');
+    playerContainer.innerHTML = template
+  } catch (err) {
+    console.error("Uh oh, trouble rendering players!", err);
+  }
 };
-
+// function render(pokemon) {
+//     const template = pokemon.map(pokemon => {
+//       return (
+//         `<li>
+//           <h2>${pokemon.name}</h2>
+//           <button class="delete-button">Delete Pokemon</button>
+//           <button class="details-button" data-name=${pokemon.name}>Get Info</button>
+//         </li>`
+//       )
+//     }).join('');
+//     pokemonListEl.innerHTML = template;
+//   }
 
 /**
  * It renders a form to the DOM, and when the form is submitted, it adds a new player to the database,
  * fetches all players from the database, and renders them to the DOM.
  */
 const renderNewPlayerForm = () => {
-    try {
-        
-    } catch (err) {
-        console.error('Uh oh, trouble rendering the new player form!', err);
-    }
-}
+  try {
+  } catch (err) {
+    console.error("Uh oh, trouble rendering the new player form!", err);
+  }
+};
 
 const init = async () => {
-    const players = await fetchAllPlayers();
-    renderAllPlayers(players);
+  const players = await fetchAllPlayers();
+  console.log(players)
+  renderAllPlayers(players);
 
-    renderNewPlayerForm();
-}
+  renderNewPlayerForm();
+};
 
 init();
