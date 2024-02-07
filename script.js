d5ec89031af3e72cad3412f5f4e96def42732771
Monica M <126627532+MoByte26@users.noreply.github.com>
2024-02-06 20:47:42 -0600

    Javascript updated

--- script.js
+++ script.js
@@ -11,12 +11,12 @@ const APIURL = `https://fsa-puppy-bowl.herokuapp.com/api/${cohortName}/`;
  * @returns An array of objects.
  */
 const fetchAllPlayers = async () => {
-  try { 
+  try {
     // const response = await fetch(`${BASE_URL}/${name}`);
     const response = await fetch(`${APIURL}/players`);
     const result = await response.json();
     // console.log(result)
-    return result.data.players
+    return result.data.players;
   } catch (err) {
     console.error("Uh oh, trouble fetching players!", err);
   }
@@ -30,7 +30,22 @@ const fetchSinglePlayer = async (playerId) => {
 };
 
 const addNewPlayer = async (playerObj) => {
+  
+  // const string1 = 'hello'
+  // const string2 = 'monica';
+  // const string3 = string1 + ' ' + string2; // hello monica // String Concatenation
+  // const string4 = `${string1} + ${string2}` // hello monica // Template Literal;
+
   try {
+    const newPlayerResponse = await fetch(`${APIURL}/players`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      }, 
+      body: JSON.stringify(playerObj)
+    }
+  );
+    const result = await newPlayerResponse.json();
   } catch (err) {
     console.error("Oops, something went wrong with adding that player!", err);
   }
@@ -38,6 +53,15 @@ const addNewPlayer = async (playerObj) => {
 
 const removePlayer = async (playerId) => {
   try {
+    const response = await fetch(
+      // https://fsa-puppy-bowl.herokuapp.com/api/${cohortName}/players/${WHATEVER THE PLAYER ID ENDS UP BEING}
+        `${APIURL}/players/${playerId}`,
+        {
+          method: 'DELETE',
+        }
+      );xs
+      const result = await response.json();
+      console.log(result);
   } catch (err) {
     console.error(
       `Whoops, trouble removing player #${playerId} from the roster!`,
@@ -67,31 +91,42 @@ const removePlayer = async (playerId) => {
  * @returns the playerContainerHTML variable.
  */
 const renderAllPlayers = (playerList) => {
-  try { 
-    const template = playerList.map(player => {
-        return (
-            `<section>
-            <h2>${player.name}</h2>
-            </section>`
-        )
-    }).join('');
-    playerContainer.innerHTML = template
+  try {
+    const template = playerList
+      .forEach(({id, name, breed, status, imageUrl, teamId, cohortId}) => {
+        //SINGLE PUPPY CONTAINER
+        const puppyContainer = document.createElement('div');
+        puppyContainer.innerHTML =`
+        <section>
+          <img src="${imageUrl}"/>
+          <p>${imageUrl} currentUrl passed from API</p>
+            <h2>id: ${id}</h2>
+            <h2>name: ${name}</h2>
+            <h2>breed: ${breed}</h2>
+            <h2>status: ${status}</h2>
+            <h2>teamId: ${teamId}</h2>
+            <h2>cohortId: ${cohortId}</h2>
+            </section>`;
+            //ALL PLAYERS
+            playerContainer.append(puppyContainer);
+            const deleteButton = document.createElement('button');
+            //DELETE BUTTON FOR SPECIFIC PUPPY 
+            deleteButton.textContent = 'DELETE'
+            //ADD CALLBACK TO DELETE BUTTON AND PASS IN ID SOMEHOW
+            deleteButton.addEventListener('click', () => {
+            //   console.log({name, id, status})
+            removePlayer(id)
+            })
+            puppyContainer.append(deleteButton)
+      })
+
+
+      // .join("");
+    // playerContainer.innerHTML = template;
   } catch (err) {
     console.error("Uh oh, trouble rendering players!", err);
   }
 };
-// function render(pokemon) {
-//     const template = pokemon.map(pokemon => {
-//       return (
-//         `<li>
-//           <h2>${pokemon.name}</h2>
-//           <button class="delete-button">Delete Pokemon</button>
-//           <button class="details-button" data-name=${pokemon.name}>Get Info</button>
-//         </li>`
-//       )
-//     }).join('');
-//     pokemonListEl.innerHTML = template;
-//   }
 
 /**
  * It renders a form to the DOM, and when the form is submitted, it adds a new player to the database,
@@ -99,6 +134,38 @@ const renderAllPlayers = (playerList) => {
  */
 const renderNewPlayerForm = () => {
   try {
+    //USING JS TO CREATE A FORM
+    const formElement = document.createElement('form');
+const formTemplate = `
+<label>name</label>
+<input name="name"/>
+<label>breed</label>
+<input name="breed"/>
+<label>imgurl</label>
+<input name="imgurl"/>
+<label for="status"> Select a Status:</label>
+<select name="status">
+<option value="">SELECT STATUS</option>
+<option value="field">field</option>
+<option value="bench">bench</option>
+</select>
+<button type="submit"> SUBMIT </button
+`
+//SETTING INNER HTML TO OUR TEMPLATE
+formElement.innerHTML = formTemplate;
+//ADDING EVENT LISTENER
+formElement.addEventListener(('submit'), (event) => {
+event.preventDefault();
+const name = event.target.name.value;
+const breed = event.target.breed.value;
+const status = event.target.status.value;
+const imgUrl = event.target.imgurl.value;
+addNewPlayer({name,breed,status, imgUrl: imgUrl});
+
+})
+
+//THIS IS THE DIV CONTAINING THE FORM
+newPlayerFormContainer.append(formElement)
   } catch (err) {
     console.error("Uh oh, trouble rendering the new player form!", err);
   }
@@ -106,7 +173,7 @@ const renderNewPlayerForm = () => {
 
 const init = async () => {
   const players = await fetchAllPlayers();
-  console.log(players)
+  console.log(players);
   renderAllPlayers(players);
 
   renderNewPlayerForm();
